4e1223a0eac8a7e6c3765c7e85004e418532e7e3
VarunSalaria12219988 <119073114+VarunSalaria9@users.noreply.github.com>
2022-12-11 11:55:06 +0530

    Add files via upload

---
+++ lecture10.py
@@ -0,0 +1,18 @@
+print("hello \"world\" world")
+print('I\'m Sagar')
+
+print("line A\nline B\n line C")
+
+
+print("name\tSagar")
+
+print("this is backslash \\")
+print("this is double backslash \\\\")
+
+print("hell\blo")   
+
+
+
+
+
+
